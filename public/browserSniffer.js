346c82e0dbcb637686d6823712e0fd6cf65eeb5c
Alexrmsu <alexmagnasura@gmail.com>
2024-08-19 20:59:11 -0400

    entropy values

--- public/browserSniffer.js
+++ public/browserSniffer.js
@@ -1,14 +1,36 @@
-async function getEntropyValues(){
-    const navigatorTester = globalThis.navigator? globalThis.navigator : {};
-
+function getEntropyValues() {
+    navigator.userAgentData
+        .getHighEntropyValues([
+            "architecture",
+            "bitness",
+            "formFactor",
+            "model",
+            "platform",
+            "platformVersion",
+            "uaFullVersion",
+            "wow64"
+        ])
+        .then((values) => {
+            document.getElementById('architecture').textContent = values.architecture;
+            document.getElementById('bitness').textContent = values.bitness;
+            document.getElementById('formFactor').textContent = values.formFactor;
+            document.getElementById('model').textContent = values.model;
+            document.getElementById('platformUA').textContent = values.platform;
+            document.getElementById('platformVersion').textContent = values.platformVersion;
+            document.getElementById('uaFullVersion').textContent = values.uaFullVersion;
+            document.getElementById('wow64').textContent = values.wow64;
+        });
 }
 
 
 window.onload = function() {
+    getEntropyValues();
     document.getElementById('browserName').textContent = navigator.appName;
     document.getElementById('browserVersion').textContent = navigator.appVersion;
     document.getElementById('platform').textContent = navigator.platform;
     document.getElementById('userAgent').textContent = navigator.userAgent;
+
+
 
 };
 
